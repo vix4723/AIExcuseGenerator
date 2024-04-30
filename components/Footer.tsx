88b9b08d2ebaa8faf222d8fef808f8bc66229aa0
Vix <63135033+vix4723@users.noreply.github.com>
2024-04-30 04:09:29 +0200

    updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,29 +3,9 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
-      <div>
-        Powered by{' '}
-        <a
-          href="https://mistral.ai/news/mixtral-of-experts"
-          target="_blank"
-          rel="noreferrer"
-          className="font-bold hover:underline transition underline-offset-2"
-        >
-          Mixtral{' '}
-        </a>
-        and{' '}
-        <a
-          href="https://platform.openai.com/docs/models"
-          target="_blank"
-          rel="noreferrer"
-          className="font-bold hover:underline transition underline-offset-2"
-        >
-          GPT-3.5
-        </a>
-      </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://twitter.com/nutlope"
+          href="https://twitter.com/vixclotet"
           className="group"
           aria-label="TaxPal on Twitter"
         >
@@ -37,7 +17,7 @@ export default function Footer() {
           </svg>
         </Link>
         <Link
-          href="https://github.com/Nutlope/twitterbio"
+          href="https://github.com/vix4723/PixelArtProfilePicGen"
           className="group"
           aria-label="TaxPal on GitHub"
         >
