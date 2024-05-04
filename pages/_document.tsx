3f490660eb82e84aae655e51b73b632fd8478a0a
Vix <63135033+vix4723@users.noreply.github.com>
2024-05-04 05:41:40 +0200

    updated the document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,22 +5,22 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.png" />
+          <link rel="icon" href="/favicon.jpg" />
           <meta
             name="description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your best excuse to avoid your next commitment"
           />
-          <meta property="og:site_name" content="twitterbio.io" />
+          <meta property="og:site_name" content="https://excusegeneratorio.vercel.app/" />
           <meta
             property="og:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your best excuse to avoid your next commitment"
           />
-          <meta property="og:title" content="Twitter Bio Generator" />
+          <meta property="og:title" content="AI Excuse Generator" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
+          <meta name="twitter:title" content="AI Excuse Generator" />
           <meta
             name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your best excuse to avoid your next commitment"
           />
           <meta
             property="og:image"
