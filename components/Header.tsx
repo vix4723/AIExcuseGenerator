6d4ed5550655dcb4b35171f8da02788f08970d0d
Vix <63135033+vix4723@users.noreply.github.com>
2024-05-25 21:48:15 +0200

    updated some components

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,16 +3,21 @@ import Github from './GitHub';
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
-      <Link href="/" className="flex space-x-3">
+    <header className="flex flex-col items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+      <Link href="/" className="flex space-x-5">
         <img
           alt="header text"
           src="/favicon.png"
           className="sm:w-9 sm:h-9 w-8 h-8"
         />
-        <h1 className="sm:text-3xl text-2xl font-bold mr-1 tracking-tight">
-          AI Excuse Generator
+        <h1 className="sm:text-3xl text-2xl font-bold mr-1 tracking-tight font-mono">
+          Your To-Go Website to Create your Next Excuse
         </h1>
+        <img
+          alt="header text"
+          src="/favicon.png"
+          className="sm:w-9 sm:h-9 w-8 h-8"
+        />
       </Link>
     </header>
   );
